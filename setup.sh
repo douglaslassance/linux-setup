a3bffc3fa818a8b65275dbaf37bac77f695c2be3
Douglas Lassance <douglassance@gmail.com>
2025-02-05 02:47:05 +0000

    Added more configuration linking

--- setup.sh
+++ setup.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
 # Install packages.
-# sudo dnf install kitty hyprland waybar wofi blender krita swaync hyprpaper hyprshot 
+# sudo dnf install kitty waybar wofi blender krita swaync starship
+# hyprland hyprlock hyprpaper hyprshot hypridle
 # sudo dnf install cargo vlc
 
 # Get the absolute path of the script
-script_path=$(dirname "$0")
+script_path=$(dirname $(realpath "$0"))
 
-pushd $script_path
+# Clear configurations
+rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
+rm -rf ~/.config/wofi
+rm -rf ~/.config/kitty
 
-# Link configuration files.
-ln -sf $script_path/home/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+# Link bin folder.
+chmod +x $script_path/home/.local/bin/*
+ln -sdf $script_path/home/.local/bin ~/.local
 
-popd
+# Link configurations
+ln -sdf $script_path/home/.config/hypr ~/.config
+ln -sdf $script_path/home/.config/waybar ~/.config
+ln -sdf $script_path/home/.config/wofi ~/.config
+ln -sdf $script_path/home/.config/kitty ~/.config
+
+# Get wallpapers.
+git clone https://github.com/aynp/dracula-wallpapers.git ~/Pictures/Wallpapers
